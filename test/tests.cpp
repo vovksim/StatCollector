d1a03303d205f841543cc7bc9ec72498786fd7bd
Ivan Mykhailov <ivan.mykhailov11@gmail.com>
2024-07-05 14:50:46 +0300

    Added overflow, even and odd data tests for Median.

--- test/tests.cpp
+++ test/tests.cpp
@@ -3,6 +3,17 @@
 //
 #include "../src/stats.hpp"
 #include <cassert>
+#include <random>
+
+void gen_random(std::vector<int> &r, std::size_t length) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> distrib(std::numeric_limits<int>::min(), std::numeric_limits<int>::max());
+    r.assign(length, 0);
+    for (auto &value: r) {
+        value = distrib(gen);
+    }
+}
 
 void emptyDataTest() {
     std::vector<int> emptyVector;
@@ -21,6 +32,30 @@ void emptyDataTest() {
     assert(median == std::nullopt);
 }
 
+void oddDataSizeMedian() {
+    std::vector<int> oddDataSize{234, 6, 45, 2, 12, 31};
+    auto median = st::calcMean(oddDataSize);
+    assert(median == 21.5);
+}
+
+void evenDataSizeMedian() {
+    std::vector<int> oddDataSize{8, 6, 3, 21, 243, 64, 6};
+    auto median = st::calcMean(oddDataSize);
+    assert(median == 8);
+}
+
+void overflowRiskMedian() {
+    std::vector<int> oddDataSize;
+    oddDataSize.assign(10, std::numeric_limits<int>::max());
+    auto median = st::calcMean(oddDataSize);
+    assert(median.value() == std::numeric_limits<int>::max());
+}
+
+
 int main() {
     emptyDataTest();
+    oddDataSizeMedian();
+    evenDataSizeMedian();
+    overflowRiskMedian();
+
 }
